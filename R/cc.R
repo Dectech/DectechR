5dfbf96c7de0becfd2bf0afddee2574287120187
Keith Simpson <Keith Simpson>
2016-02-22 12:42:29 +0000

    initial commit

---
+++ R/cc.R
@@ -0,0 +1,89 @@
+cc <- function(data,destination="clipboard-128"){
+    write.table(data,destination,sep="\t",col.names=NA)
+}
+
+cc2 = function(data, destination = NA, includeRowNames = FALSE){
+    ####################################################
+    ### Fucntion to write an object to the clipboard ###
+    ###  ...making allowance for nested tables       ###
+    ####################################################
+
+    #--(1) If file name given, write directly to file, otherwise will write to clipboard...
+    if (!is.na(destination)){
+
+        # if file name given then write to file...
+        write.table(data,destination,sep="\t",row.names=F)
+
+    }else{
+        #---(2) will use the "writeClipboard" function, however...
+        # ...this function only takes strings, so need to format data
+
+        # if desired, save the row names...
+        if (includeRowNames == TRUE){
+            originalRowNames = rownames(data)
+        }
+
+
+        #---(3) we will treat nested tables differently, so check status..
+        isNestedTable = FALSE
+        if (class(data) == "table"){
+            # only care if there is more than one dimension
+            if (length(dim(data))>1){
+                isNestedTable = TRUE
+            }
+        }
+
+        #---(4) convert all objects to dataframes if not already
+        if (class(data) != "data.frame"){
+            data = as.data.frame(data)
+        }
+
+        #---(5) for multi dimensional nested tables...
+        #  ...want to reshape into usable format
+        if (isNestedTable){
+            nvars = length(data)
+
+            # create an extra header row, so show name of variable that will span columns
+            extraRow = paste(array("\t",nvars-2),collapse="")
+            extraRow = paste0(extraRow,names(data)[nvars-1])
+
+
+            #-- reshape so that last variable is split into columns...
+            # first get some details about current shape...
+            numCols = ncol(data)
+            myValues = names(data)[numCols]
+            myColumnVar = names(data)[numCols-1]
+            myColumnVarNames = as.character(unique(data[,myColumnVar]))
+            myOtherVars = names(data)[-c(numCols-1,numCols)]
+
+            # now reshape so "myColumnVar" is switched to wide format...
+            data=reshape(data,v.names=myValues,timevar = myColumnVar, idvar=myOtherVars,direction="wide")
+            # ...and use orginal levels for column headings
+            names(data) = c(myOtherVars,myColumnVarNames)
+
+        }
+
+        #---(6) get column headings for table...
+
+        colHeadings = paste(names(data),collapse="\t")
+        if (isNestedTable){
+            colHeadings = append(extraRow,colHeadings)
+        }
+
+        #---(7) Go through every row, make it a string sep. by tabs..
+        outputVector=apply(data,1,paste,collapse="\t")
+        outputVector = append(colHeadings,outputVector)
+
+        #--- if desired, include original row names....
+        if((includeRowNames == TRUE)&(isNestedTable == FALSE)){
+            NextraRows = length(outputVector) - length(originalRowNames)
+            originalRowNames = append(rep("",NextraRows),originalRowNames)
+            outputVector = paste0(originalRowNames,"\t",outputVector)
+        }
+
+
+
+        #---(8) then write to clipboard
+        writeClipboard(outputVector)
+    }
+}
---
+++ R/dectechXmlToDataframe.R
@@ -0,0 +1,142 @@
+dectechXmlToDataframe <- function(filePath, removeIncompletes = TRUE, saveLabels = TRUE, dropTimeStamps = TRUE){
+    ################################################################################################
+    ##  Function to convert an XML file exported from GlobalPark/Questback into an R data frame   ##
+    ################################################################################################
+
+    startTime = proc.time()
+
+    # ----- (0) output for user   -----------------------------------
+    print("------- GlobalPark XML to dataframe ----------------")
+    print("-   With the following options:                    -")
+    if (removeIncompletes){
+        print("-       Remove incompetes: Yes                     -")
+    }else{
+        print("-       Remove incompetes: No                      -")
+    }
+    if (saveLabels){
+        print("-       Save labels as an attribute: Yes           -")
+    }else{
+        print("-       Save labels as an attribute: No            -")
+    }
+    if (dropTimeStamps){
+        print("-       Drop GP time stamp variables: Yes          -")
+    }else{
+        print("-       Drop GP time stamp variables: No           -")
+    }
+    print("----------------------------------------------------")
+
+    # ----- (1) make sure XML package is loaded (install if not available) --------------------
+
+    #if (("XML" %in% rownames( installed.packages() ) ) == FALSE){
+    #    install.packages("XML")
+    #}
+    #require(XML)
+
+
+    # ----- (2) load xml file and get variable names/labels/type  --------
+
+    data <- XML::xmlTreeParse(filePath, useInternalNodes = TRUE,
+                              options = (XML::HUGE)|(XML::RECOVER), encoding = "UTF-8")
+
+
+    print("---> Extracting variable labels...")
+    vars = XML::getNodeSet(data, "//variable[@name]")
+    varNames = sapply(vars, function(x) XML::xmlGetAttr(x, "name"))
+    varType = sapply(vars, function(x) XML::xmlGetAttr(x, "type"))
+    varLabels = XML::xpathSApply(data,"//variable[@name]/label[text()]",XML::xmlValue)
+
+
+    # some nodes also have a "code" that define the levels of the variable...
+    varsWithCode = XML::getNodeSet(data,"//variable/codes/parent::*")
+    varNamesWithCode = sapply(varsWithCode, function(x) XML::xmlGetAttr(x, "name"))
+
+
+    # the fastest way to get variable labels is to extract all of them first
+    # ...however this will be one long undifferentiated vector of names
+
+    rawKeyList = XML::xpathSApply(data,"//variable[@name]/codes/code",XML::xmlGetAttr,"key")
+    rawLabelList = XML::xpathSApply(data,"//variable[@name]/codes/code",XML::xmlValue)
+    # ...therefore we will also need an index to link labesl back to their variable...
+    # ...this will be vector of the appropriate break points in rawKeyList and rawLabelList
+    rawLabelListIndex  = c(0,cumsum(sapply(varsWithCode, function(x){
+        length(XML::xmlChildren(XML::xmlChildren(x)$codes))
+    })))
+
+
+    #--- (3) convert to data frame....
+
+    if (removeIncompletes == TRUE){
+        print("---> Dropping incompletes...")
+        # questback completes are numbered 31 and 32
+        mainXMLData = XML::getNodeSet(data,"//row/dispcode[text()=31 or text()=32]/parent::*")
+    }else{
+        mainXMLData = XML::getNodeSet(data,"//row")
+    }
+
+
+    print("---> Getting raw data (this bit can take a few mins!)")
+
+    mainData = XML::xmlToDataFrame(mainXMLData,homogeneous=T,nodes=varNames,collectNames=F)
+    print("  -->time so far (minutes):")
+    print((proc.time() - startTime)/60)
+
+
+    # ----- (4) go through each variable....  ---------------------
+    # -----   (a) if it has a code, make it into an R factor...
+    # -----   (b) if not, convert to the appropriate type...
+
+    print("---> Matching labels to values...")
+
+    for (v in varNames){
+        if (v %in% varNamesWithCode){ #--- (a) if has a code....
+
+
+            #--- get the keys and labels for this variable....
+
+            vi = which(varNamesWithCode == v)
+            thisKey = rawKeyList[(rawLabelListIndex[vi]+1):rawLabelListIndex[vi+1]]
+            thisLabels = rawLabelList[(rawLabelListIndex[vi]+1):rawLabelListIndex[vi+1]]
+
+            #--- sometimes levels will not be in original order...
+            mainData[,v] = factor(mainData[,v],
+                                  levels=sort(as.numeric(levels(mainData[,v]))))
+
+            #--- map labels onto values....
+            if (sum(table(thisLabels)>1)>0){
+                thisLabels = paste(thisKey,thisLabels)
+            }
+            mainData[,v] = factor(mainData[,v],levels=thisKey,
+                                  labels=thisLabels)
+
+
+        }else{#--- (b) if does not have a code....
+            #...then doesn't need to be factor...
+            thisVarType = varType[which(varNames==v)]
+            if (thisVarType %in% c("integer","decimal")){
+                mainData[,v] = as.numeric(as.character(mainData[,v]))
+            }else if (thisVarType %in% c("character","blob")){
+                mainData[,v] = as.character(mainData[,v])
+            }
+        }
+    }
+
+    print("  -->time so far (minutes):")
+    print((proc.time() - startTime)/60)
+
+
+    if (dropTimeStamps == TRUE){
+        print("---> Removing timestamps...")
+        mainData = mainData[,(varType != "relative timestamp")]
+        varLabels = varLabels[(varType != "relative timestamp")]
+    }
+
+
+    print("---> Finished...")
+
+    if (saveLabels==TRUE){
+        attr(mainData,"labels") = varLabels
+        print("   ...to get GP labels type something like: attr(df,'labels')")
+    }
+
+    return(mainData)
+}
