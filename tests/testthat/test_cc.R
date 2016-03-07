f9cf5ec5d25670980ddd14dfdf8b3fd04b0b3fe2
Keith Simpson <k.simpson@dectech.co.uk>
2016-03-07 18:13:18 +0000

    add some test functions for cc2

--- tests/testthat/test_cc.R
+++ tests/testthat/test_cc.R
@@ -1,6 +1,6 @@
 library(DectechR)
 
-context("test CC")
+context("test cc()")
 
 test_that("cc() gives an error when nothing sent", {
     expect_error(cc())
@@ -52,5 +52,61 @@ test_that("cc() output as expected for 3 way table", {
     expect_equal(nrow(reformedTable), 55)
     expect_equal(ncol(reformedTable), 5)
 })
+
+
+context("test cc2()")
+
+test_that("cc2() gives an error when nothing sent", {
+    expect_error(cc2())
+})
+
+test_that("cc2() writing something to clipboard", {
+    cc2(1)
+    ccContents = readClipboard()
+
+    expect_identical(ccContents[1],  "data")
+    expect_identical(ccContents[2], "1")
+})
+
+test_that("cc2() output as expected for 1 way table", {
+    tab1 = with(mtcars, table(gear))
+    cc2(tab1)
+    ccContents = readClipboard()
+
+    expect_equal(length(ccContents), 4)
+    expect_identical(ccContents[1], "gear\tFreq")
+    expect_identical(ccContents[4], "5\t 5")
+})
+
+
+test_that("cc2() output as expected for 2 way table", {
+    tab2 = with(mtcars, table(carb,gear))
+    cc2(tab2)
+    ccContents = readClipboard()
+    reformedTable = t(sapply(ccContents[-1],function(x){strsplit(x,"\t")[[1]]}))
+
+    expect_equal(length(ccContents), 8)
+    expect_identical(ccContents[1], "\tgear")
+    expect_identical(ccContents[2], "carb\t3\t4\t5")
+    expect_equal(nrow(reformedTable), 7)
+    expect_equal(ncol(reformedTable), 4)
+})
+
+
+test_that("cc2() output as expected for 3 way table", {
+    tab3 = with(mtcars, table(cyl,carb,gear))
+    cc2(tab3)
+    ccContents = readClipboard()
+    reformedTable = t(sapply(ccContents[-1],function(x){strsplit(x,"\t")[[1]]}))
+
+
+    expect_equal(length(ccContents), 20)
+    expect_identical(ccContents[1], "\t\tgear")
+    expect_identical(ccContents[16], "6\t6\t0\t0\t1")
+    expect_equal(nrow(reformedTable), 19)
+    expect_equal(ncol(reformedTable), 5)
+})
+
+
 
 
