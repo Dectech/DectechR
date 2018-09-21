d03b2f8f7ba77d3f089da55769732066601e9ad3
Keith Simpson <k.simpson@dectech.co.uk>
2018-09-21 14:05:10 +0100

    (minor) updated variable names in runUnivariate to match style guide

--- R/runUnivariate.R
+++ R/runUnivariate.R
@@ -4,8 +4,8 @@ runUnivariate <- function(mod, ...) UseMethod("runUnivariate")
 runUnivariate.lm <- function(mod, returnIntercept = FALSE) { #mod = m1
     # run univariate regression for each input variable...
     # get all IV's in original call...
-    IVList <- attr(mod$terms , "term.labels")
-    DVname <- (mod$terms[[2]])
+    IV_list <- attr(mod$terms , "term.labels")
+    DV_name <- (mod$terms[[2]])
 
     if (returnIntercept == TRUE) {
         intercept_terms <- "(Intercept)"
@@ -14,55 +14,55 @@ runUnivariate.lm <- function(mod, returnIntercept = FALSE) { #mod = m1
     }
 
     #---- loop through each variable...----------------
-    resultMatrix <- NULL
-    for (var in IVList) { # var <- IVList[2]
+    result_matrix <- NULL
+    for (var in IV_list) { # var <- IV_list[2]
         # construct a formula for each variable and run model...
-        thisFormula <- as.formula(paste0(DVname," ~ `", var,"`")) # NB: include [`] to deal with variables declared as "factor(var)" etc.
+        this_formula <- as.formula(paste0(DV_name," ~ `", var,"`")) # NB: include [`] to deal with variables declared as "factor(var)" etc.
 
-        thisMod <- lm(thisFormula, data = mod$model)
+        this_mod <- lm(this_formula, data = mod$model)
 
         # extract the relevant information (beta, p-value)...
-        coeffTable <- summary(thisMod)$coefficients
+        coeff_table <- summary(this_mod)$coefficients
 
         # if it was a factor variable, create a row for each level...
         # ...only include the intercept once (as a way of indicating that it is a factor)
         # ...but perhaps there is a better way of doing this??
 
-        thisResult <- NULL
+        this_result <- NULL
 
-        thisIntercept <- coeffTable[1,"Estimate"]
-        for (i in 2:nrow(coeffTable)) {
+        this_intercept <- coeff_table[1,"Estimate"]
+        for (i in 2:nrow(coeff_table)) {
             if (returnIntercept == TRUE) {
-                thisSubResult <- c(rownames(coeffTable)[i], thisIntercept,coeffTable[i,"Estimate"],coeffTable[i,"Pr(>|t|)"])
+                this_subresult <- c(rownames(coeff_table)[i], this_intercept,coeff_table[i,"Estimate"],coeff_table[i,"Pr(>|t|)"])
             } else {
-                thisSubResult <- c(rownames(coeffTable)[i], coeffTable[i,"Estimate"],coeffTable[i,"Pr(>|t|)"])
+                this_subresult <- c(rownames(coeff_table)[i], coeff_table[i,"Estimate"],coeff_table[i,"Pr(>|t|)"])
             }
-            thisResult <- rbind(thisResult,thisSubResult)
-            thisIntercept <- NA
+            this_result <- rbind(this_result, this_subresult)
+            this_intercept <- NA
         }
 
 
-        resultMatrix <- rbind(resultMatrix, thisResult)
+        result_matrix <- rbind(result_matrix, this_result)
     }
 
     #---- format output -------------------
-    rownames(resultMatrix) <- 1:nrow(resultMatrix)
-    resultMatrix <- as.data.frame(resultMatrix,stringsAsFactors = FALSE)
-    names(resultMatrix) <- c("IV",intercept_terms,"beta","p-value")
+    rownames(result_matrix) <- 1:nrow(result_matrix)
+    result_matrix <- as.data.frame(result_matrix, stringsAsFactors = FALSE)
+    names(result_matrix) <- c("IV", intercept_terms, "beta", "p-value")
 
-    resultMatrix[,-1] <- data.matrix(resultMatrix[,-1])
+    result_matrix[,-1] <- data.matrix(result_matrix[,-1])
 
-    resultMatrix[,1] <- gsub("`","",resultMatrix[,1])
+    result_matrix[,1] <- gsub("`", "", result_matrix[,1])
 
-    return(resultMatrix)
+    return(result_matrix)
 }
 
 runUnivariate.glm <- function(mod, returnIntercept = FALSE) {
     # run univariate regression for each input variable...
 
     # get all IV's in original call...
-    IVList <- attr(mod$terms , "term.labels")
-    DVname <- (mod$terms[[2]])
+    IV_list <- attr(mod$terms , "term.labels")
+    DV_name <- (mod$terms[[2]])
     model_family <- as.character(mod$call["family"])
 
     if (returnIntercept == TRUE) {
@@ -72,57 +72,57 @@ runUnivariate.glm <- function(mod, returnIntercept = FALSE) {
     }
 
     #---- loop through each variable...----------------
-    resultMatrix <- NULL
-    for (var in IVList) { # var <- IVList[1]
+    result_matrix <- NULL
+    for (var in IV_list) { # var <- IV_list[1]
         # construct a formula for each variable and run model...
-        thisFormula <- as.formula(paste0(DVname," ~ `", var,"`")) # NB: include [`] to deal with variables declared as "factor(var)" etc.
+        this_formula <- as.formula(paste0(DV_name," ~ `", var,"`")) # NB: include [`] to deal with variables declared as "factor(var)" etc.
 
-        thisMod <- glm(thisFormula, data = mod$model, family = model_family)
+        this_mod <- glm(this_formula, data = mod$model, family = model_family)
 
 
         # extract the relevant information (beta, p-value)...
-        coeffTable <- summary(thisMod)$coefficients
+        coeff_table <- summary(this_mod)$coefficients
 
         # if it was a factor variable, create a row for each level...
         # ...only include the intercept once (as a way of indicating that it is a factor)
         # ...but perhaps there is a better way of doing this??
 
-        thisResult <- NULL
-        thisIntercept <- coeffTable[1,"Estimate"]
-        for (i in 2:nrow(coeffTable)) {
+        this_result <- NULL
+        this_intercept <- coeff_table[1,"Estimate"]
+        for (i in 2:nrow(coeff_table)) {
             if (returnIntercept == TRUE) {
-                thisSubResult <- c(rownames(coeffTable)[i], thisIntercept,coeffTable[i,"Estimate"],coeffTable[i,"Pr(>|z|)"])
+                this_subresult <- c(rownames(coeff_table)[i], this_intercept, coeff_table[i,"Estimate"],coeff_table[i,"Pr(>|z|)"])
             } else {
-                thisSubResult <- c(rownames(coeffTable)[i], coeffTable[i,"Estimate"],coeffTable[i,"Pr(>|z|)"])
+                this_subresult <- c(rownames(coeff_table)[i], coeff_table[i,"Estimate"], coeff_table[i,"Pr(>|z|)"])
             }
-            thisResult <- rbind(thisResult,thisSubResult)
-            thisIntercept <- NA
+            this_result <- rbind(this_result,this_subresult)
+            this_intercept <- NA
         }
 
 
 
-        resultMatrix <- rbind(resultMatrix, thisResult)
+        result_matrix <- rbind(result_matrix, this_result)
     }
 
     #---- format output -------------------
-    rownames(resultMatrix) <- 1:nrow(resultMatrix)
-    resultMatrix <- as.data.frame(resultMatrix,stringsAsFactors = FALSE)
-    names(resultMatrix) <- c("IV",intercept_terms,"beta","p-value")
+    rownames(result_matrix) <- 1:nrow(result_matrix)
+    result_matrix <- as.data.frame(result_matrix,stringsAsFactors = FALSE)
+    names(result_matrix) <- c("IV",intercept_terms,"beta","p-value")
 
-    resultMatrix[,-1] <- data.matrix(resultMatrix[,-1])
+    result_matrix[,-1] <- data.matrix(result_matrix[,-1])
 
-    resultMatrix[,1] <- gsub("`","",resultMatrix[,1])
+    result_matrix[,1] <- gsub("`","",result_matrix[,1])
 
-    return(resultMatrix)
+    return(result_matrix)
 }
 
 runUnivariate.polr <- function(mod, returnIntercept = FALSE) { # mod = ol1
     # run univariate regression for each input variable...
 
     # get all IV's in original call...
-    IVList <- attr(mod$terms , "term.labels")
-    DVname <- (mod$terms[[2]])
-    DV_levels <- levels(mod$model[,which(colnames(mod$model) == DVname)])
+    IV_list <- attr(mod$terms , "term.labels")
+    DV_name <- (mod$terms[[2]])
+    DV_levels <- levels(mod$model[,which(colnames(mod$model) == DV_name)])
     n_levels <- length(DV_levels)
 
 
@@ -133,21 +133,21 @@ runUnivariate.polr <- function(mod, returnIntercept = FALSE) { # mod = ol1
     }
 
     #---- loop through each variable...----------------
-    resultMatrix <- NULL
-    for (var in IVList) { # var <- IVList[1]
+    result_matrix <- NULL
+    for (var in IV_list) { # var <- IV_list[1]
 
         # construct a formula for each variable and run model...
-        thisFormula <- as.formula(paste0(DVname," ~ `", var,"`")) # NB: include [`] to deal with variables declared as "factor(var)" etc.
+        this_formula <- as.formula(paste0(DV_name," ~ `", var,"`")) # NB: include [`] to deal with variables declared as "factor(var)" etc.
 
-        thisMod <- polr(thisFormula, data = mod$model,Hess = T)
+        this_mod <- polr(this_formula, data = mod$model,Hess = T)
 
 
         # extract the relevant information (beta, p-value)...
-        coeffTable <- DectechR::getOutputTable(thisMod)
-        coeffTable <- coeffTable[["out_table"]]
+        coeff_table <- DectechR::getOutputTable(this_mod)
+        coeff_table <- coeff_table[["out_table"]]
 
 
-        n_iv_levels <- nrow(coeffTable) - (n_levels - 1)
+        n_iv_levels <- nrow(coeff_table) - (n_levels - 1)
 
 
 
@@ -155,30 +155,30 @@ runUnivariate.polr <- function(mod, returnIntercept = FALSE) { # mod = ol1
         # ...only include the intercept once (as a way of indicating that it is a factor)
         # ...but perhaps there is a better way of doing this??
 
-        thisResult <- NULL
-        thisIntercept <- coeffTable[-(1:n_iv_levels),"Value"]
+        this_result <- NULL
+        this_intercept <- coeff_table[-(1:n_iv_levels),"Value"]
 
         for (i in 1:n_iv_levels) {
             if (returnIntercept == TRUE) {
-                thisSubResult <- c(rownames(coeffTable)[i], thisIntercept,coeffTable[i,"Value"],coeffTable[i,"p value"])
+                this_subresult <- c(rownames(coeff_table)[i], this_intercept,coeff_table[i,"Value"],coeff_table[i,"p value"])
             } else {
-                thisSubResult <- c(rownames(coeffTable)[i], coeffTable[i,"Value"],coeffTable[i,"p value"])
+                this_subresult <- c(rownames(coeff_table)[i], coeff_table[i,"Value"],coeff_table[i,"p value"])
             }
-            thisResult <- rbind(thisResult,thisSubResult)
-            thisIntercept <- rep(NA,(n_levels - 1))
+            this_result <- rbind(this_result,this_subresult)
+            this_intercept <- rep(NA,(n_levels - 1))
         }
 
 
 
-        resultMatrix <- rbind(resultMatrix, thisResult)
+        result_matrix <- rbind(result_matrix, this_result)
     }
-    rownames(resultMatrix) <- 1:nrow(resultMatrix)
-    resultMatrix <- as.data.frame(resultMatrix,stringsAsFactors = FALSE)
-    names(resultMatrix) <- c("IV",intercept_terms,"beta","p-value")
+    rownames(result_matrix) <- 1:nrow(result_matrix)
+    result_matrix <- as.data.frame(result_matrix,stringsAsFactors = FALSE)
+    names(result_matrix) <- c("IV",intercept_terms,"beta","p-value")
 
-    resultMatrix[,-1] <- data.matrix(resultMatrix[,-1])
+    result_matrix[,-1] <- data.matrix(result_matrix[,-1])
 
-    resultMatrix[,1] <- gsub("`","",resultMatrix[,1])
+    result_matrix[,1] <- gsub("`","",result_matrix[,1])
 
-    return(resultMatrix)
+    return(result_matrix)
 }
